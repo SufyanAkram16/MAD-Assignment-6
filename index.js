e8e36336fda3395859c747c80c40fe65f88a9d24
sufyanakram16 <sufyanakram16@hotmail.com>
2022-09-14 09:48:08 +0500

    5 programs

--- index.js
+++ index.js
@@ -20,11 +20,18 @@
 //     i++;
 // }
 // 4. Write a ts program to print all even numbers between 1 to 100. - using while loop
-var x = 100;
-var i = 1;
-while (i <= 100) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-    i++;
-}
+// var x = 100;
+// var i = 1;
+// while (i <= 100) {
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+// 5. Write a ts program to print all odd number between 1 to 100.
+// for (let i = 1; i <= 100; i++){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
