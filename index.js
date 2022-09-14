3013f1bc9fa4eb7102c311bac079b4122dcd671c
sufyanakram16 <sufyanakram16@hotmail.com>
2022-09-14 09:43:30 +0500

    4 programs solved

---
+++ index.js
@@ -0,0 +1,30 @@
+// Assignment 6
+// 1. Write a ts program to print all natural numbers from 1 to n. - using while loop
+// var userInput:string|null = prompt("Enter a number");
+// var num:number =Number(userInput);
+//  for(let i = 1; i <= num; i++){
+//     console.log(i);
+//  }
+// 2. Write a ts program to print all natural numbers in reverse (from n to 1). - using while loop
+// var userInput:string|null = prompt("Enter a number");
+// var num:number = Number(userInput);
+// while (num >= 1) {
+//     console.log(num);
+//     num--;
+// }
+// 3. Write a ts program to print all alphabets from a to z. - using while loop
+// var string:string = "az";
+// var i:number = string.charCodeAt(0);
+// while (i <= string.charCodeAt(1)) {
+//     console.log(String.fromCharCode(i));
+//     i++;
+// }
+// 4. Write a ts program to print all even numbers between 1 to 100. - using while loop
+var x = 100;
+var i = 1;
+while (i <= 100) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    i++;
+}
